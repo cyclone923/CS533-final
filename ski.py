94c129894c19172c92f3580be0261357f9f92d96
Chengxi <yangchen@oregonstate.edu>
2018-05-21 22:35:17 -0700

    get grey scale image

--- ski.py
+++ ski.py
@@ -1,5 +1,6 @@
 import gym
 import torch
+import matplotlib.pyplot as plt
 
 
 
@@ -9,15 +10,22 @@ class simulator(object):
     def __init__(self,envName):
         self.env = gym.make(envName)
 
+    def rgb2gray(self,rgb):
+
+        r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+        gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+
+        return gray
     def simulate(self):
         for i_episode in range(2):
-            observation = self.env.reset()
-            for t in range(10000):
-                self.env.render()
+            self.env.reset()
+            for t in range(100):
+                obs = self.env.render('rgb_array')
+                obs = self.rgb2gray(obs)
+                # plt.imshow(obs, cmap='gray')
                 action = self.env.action_space.sample()
-                observation, reward, done, info = self.env.step(action)
-                # print(action, reward)
-                # print(observation)
+                _, reward, done, _ = self.env.step(action)
+
                 if done:
                     print("Episode finished after {} timesteps".format(t + 1))
                     break
